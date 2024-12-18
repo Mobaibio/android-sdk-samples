c13208825314c7e66ed9123ce4ac245cb567c64f
Yuniesky Suarez <yuniesky@mobai.bio>
2024-12-19 00:03:14 +0100

    navigate to BiometricCaptureFragment

--- app/src/main/java/com/example/biometricsdkexample/HomeFragment.kt
+++ app/src/main/java/com/example/biometricsdkexample/HomeFragment.kt
@@ -1,7 +1,16 @@
 package com.example.biometricsdkexample
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+         view.findViewById<Button?>(R.id.btn_start).setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_biometricCaptureFragment)
+        }
+    }
 }
